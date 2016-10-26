51afe317ec90e8504e2232066b48be6d0ca7e66c
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-10-26 01:59:37 -0400

    compatibility update

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 using MediaBrowser.Plugins.VuPlus.Configuration;
 
@@ -8,7 +10,7 @@ namespace MediaBrowser.Plugins.VuPlus
     /// <summary>
     /// Class Plugin
     /// </summary>
-    public class Plugin : BasePlugin<PluginConfiguration>
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
             : base(applicationPaths, xmlSerializer)
@@ -42,5 +44,17 @@ namespace MediaBrowser.Plugins.VuPlus
         /// </summary>
         /// <value>The instance.</value>
         public static Plugin Instance { get; private set; }
+
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = "vuplus",
+                    EmbeddedResourcePath = "MediaBrowser.Plugins.VuPlus.Configuration.configPage.html"
+                }
+            };
+        }
     }
 }
